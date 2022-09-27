c7cee8328405e4bd0fb25ba65b7974d32faabe68
Steven <stevengf14@gmail.com>
2022-09-27 16:18:55 -0500

    Test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [buttonColor, setButtonColor] = new useState('red');
+  const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
-  const handleClick = () => {
-    setButtonColor(newButtonColor);
-  }
-
+  const handleClickButton = () => setButtonColor(newButtonColor);
+  const handleClickCheckbox = (event) => setDisabled(event.target.checked);
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }}
-        onClick={() => setButtonColor(newButtonColor)}
+      <button 
+        style={{ backgroundColor: buttonColor }}
+        onClick={handleClickButton}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input 
+        type='checkbox'
+        id='enable-button-checkbox'
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={handleClickCheckbox} 
+      />
     </div>
   );
 }
